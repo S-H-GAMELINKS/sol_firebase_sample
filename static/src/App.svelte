e3958ec11bafe8b017ad54a1b99eddf5eebec58b
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-07-22 22:27:39 +0900

    using header.svelte

--- static/src/App.svelte
+++ static/src/App.svelte
@@ -3,14 +3,12 @@
     import Index from "./routes/index.svelte";
     import About from "./routes/about.svelte";
     import Contact from "./routes/contact.svelte";
-
+    import Header from "./components/header.svelte"
     export let url = "";
 </script>
 
 <Router url="{url}">
-    <Link to="/">Home</Link>
-    <Link to="/about">About</Link>
-    <Link to="/contact">Contact</Link>
+    <Header />
     <div>
         <Route path="/" component="{Index}" />
         <Route path="/about" component="{About}" />
