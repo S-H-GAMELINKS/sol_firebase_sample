84d7fca64aa3f0f86503e3d189b45267e8911858
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-07-24 23:56:33 +0900

    fix about.svelte

--- static/src/routes/about.svelte
+++ static/src/routes/about.svelte
@@ -3,3 +3,10 @@
 </script>
 
 <h1>{ word }</h1>
+
+<div class="jumbotron">
+    <h1 class="display-4">About</h1>
+    <p class="lead">Very Simple Chat App!</p>
+    <hr class="my-4">
+    <p>Developed by Sol(C++/Stmilus/Svelte/FireBase/Docker)</p>
+</div>
