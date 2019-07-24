717bd149aff785fd4d0c712ce6a4550e3464d9bf
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-07-24 23:58:03 +0900

    fix contact.svelte

--- static/src/routes/contact.svelte
+++ static/src/routes/contact.svelte
@@ -3,3 +3,14 @@
 </script>
 
 <h1>{ word }</h1>
+
+<div class="jumbotron">
+    <h1 class="display-4">Contact</h1>
+    <p class="lead">S.H.</p>
+    <hr class="my-4">
+    <p class="lead">mail: gamelinks007@gmail.com</p>
+    <p class="lead">github: <a href="https://github.com/S-H-GAMELINKS"></a></p>
+    <p class="lead">
+        <a class="btn btn-primary btn-lg" href="https://github.com/S-H-GAMELINKS/sol_firebase_sample" role="button">Source</a>
+    </p>
+</div>
